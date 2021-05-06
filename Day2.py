17ae6417ebe2a9893360a06f13bcf11ce48b00d5
RSHANKARI <83652749+RSHANKARI@users.noreply.github.com>
2021-05-06 18:04:29 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,41 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # int datatype
+>>> num = 5
+>>> type(num)
+<class 'int'>
+>>> #float datatype
+>>> num = 6.5
+>>> type(num)
+<class 'float'>
+>>> #string datatype
+>>> num = "conversions"
+>>> type(num)
+<class 'str'>
+>>> #bool datatype
+>>> num = 2+3
+>>> bool(num)
+True
+>>> #complex datatype
+>>> a=6
+>>> b=3
+>>> c=complex(a,b)
+>>> print(c)
+(6+3j)
+>>> num=6+3j
+>>> type(num)
+<class 'complex'>
+>>> 
+>>> #int to float
+>>> a=7
+>>> float(a)
+7.0
+>>> #float toint
+>>> a=2.34
+>>> int(a)
+2
+>>> #string with values
+>>> a=str("py")
+>>> print(a)
+py
+>>> 
---
+++ Day3 Simple Calculator.py
@@ -0,0 +1,19 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #arithmetic operators
+>>> a , b = 5 , 2
+>>> print("a+b={}". format(a+b))
+a+b=7
+>>> print("a-b={}". format(a-b))
+a-b=3
+>>> print("a*b={}". format(a*b))
+a*b=10
+>>> print("a/b={}". format(a/b))
+a/b=2.5
+>>> print("a%b={}". format(a%b))
+a%b=1
+>>> print("a//b={}". format(a//b))
+a//b=2
+>>> print("a**b={}". format(a**b))
+a**b=25
+>>> 
