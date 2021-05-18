62a6313a57e3ad49f18ad36cecda575cd5c84f1e
RSHANKARI <83652749+RSHANKARI@users.noreply.github.com>
2021-05-18 23:10:07 +0530

    Add files via upload

---
+++ Day6 List.py
@@ -0,0 +1,87 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #List min or max value
+>>> list=[5,7,9,10,11,12,45,73,1,2,5,13]
+>>> maxval=-1
+>>> minval=1000
+>>> for i in range(0, len(list), 1):
+	maxval = max(maxval,list[i])
+	minval = min(minval,list[i])
+	print("max:",maxval)
+	print("min:",minval)
+
+	
+max: 5
+min: 5
+max: 7
+min: 5
+max: 9
+min: 5
+max: 10
+min: 5
+max: 11
+min: 5
+max: 12
+min: 5
+max: 45
+min: 5
+max: 73
+min: 5
+max: 73
+min: 1
+max: 73
+min: 1
+max: 73
+min: 1
+max: 73
+min: 1
+>>> list=[5,7,9,10,11,12,45,73,1,2,5,13]
+>>> maxval=0
+>>> minval=1000
+>>> for i in range(0, len(list), 1):
+	maxval = max(maxval,list[i])
+	minval = min(minval,list[i])
+	print("max:",maxval)
+	print("min:",minval)
+
+	
+max: 5
+min: 5
+max: 7
+min: 5
+max: 9
+min: 5
+max: 10
+min: 5
+max: 11
+min: 5
+max: 12
+min: 5
+max: 45
+min: 5
+max: 73
+min: 5
+max: 73
+min: 1
+max: 73
+min: 1
+max: 73
+min: 1
+max: 73
+min: 1
+>>> #palinrome program
+>>> n=(input("the value of n is:")
+   x=[]
+   
+SyntaxError: invalid syntax
+>>> 
+KeyboardInterrupt
+>>> 
+>>> x=[]
+>>> y=[]
+>>> n=(input("the value of n is:")
+
+   for i in n:
+   
+SyntaxError: invalid syntax
+>>> 
